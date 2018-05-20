782ed8cd3fafe16410a60a127fc3f9ed9e6b8ea7
jun266a <jun266a@foxmail.com>
2018-05-20 16:20:23 +0800

    将页面部署到EXPRESS的静态文件夹里，实现在NODEJS网络服务中查看静态页面

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 'use strict';
 
-const sha = require('sha1');
 const express = require('express');
-const wechat = require('node-wechat');
-const router = express.Router();
+const bodyParser = require('body-parser');
+//const router = express.Router();
 
 let app = express();
 
-app.set('views','/webapp');
-app.set('view engine','ejs');
-
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
-app.use(cookieParser());
-app.use(express.static('/webapp'));
-
-
-
+app.use(bodyParser.urlencoded({extended : true}));
+//app.use(cookieParser());
+app.use(express.static('./webapp'));
 
 //路由处理
-app.use('/index',index);
-app.use('/users',users);
-app.use('/wechat',wechat);
+
+
 
 app.listen(80,function(err){
 	if(err){
@@ -30,5 +21,3 @@ app.listen(80,function(err){
 	}
 	console.log("server is started!");
 });
-
-
